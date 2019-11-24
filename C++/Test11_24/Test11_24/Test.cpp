e22d3c558f79714030424f299fee78600b9a20a8
baosongshan <baosongshan2006@163.com>
2019-11-24 16:59:45 +0800

    add 类与对象

--- C++/Test11_24/Test11_24/Test.cpp
+++ C++/Test11_24/Test11_24/Test.cpp
@@ -1,8 +1,334 @@
 #include<iostream>
 #include<stdio.h>
-#include"fun.h"
+//#include"fun.h"
 using namespace std;
 
+// 1 常方法的const 怎么了
+// 2 GetData() 与 GetData()const 能否共存
+// 3 拷贝构造函数的const 
+// 4 赋值语句 
+//   Test& operator=(const Test &t)
+//   void  operator=(Test t)
+
+class Test
+{
+public:
+	//构造函数
+	//Test t;
+	Test(int data = 0)
+	{
+		cout<<"Create Test Object:"<<this<<endl;
+		m_data = data;
+	}
+	//拷贝构造函数
+	//语法要求 语义要求
+	Test(Test &t)
+	{
+		cout<<"Copy Create Test Object:"<<this<<endl;
+		m_data = t.m_data;
+	}
+	//赋值语句
+	/*
+	Test& operator=(const Test &t)
+	{
+		cout<<"Assign,"<<this<< "="<<&t<<endl; 
+		if(this != &t)
+		{
+			m_data = t.m_data;
+		}
+		return *this;
+	}
+	*/
+	void operator=(Test t)
+	{
+		cout<<"Assign,"<<this<< "="<<&t<<endl; 
+		if(this != &t)
+		{
+			m_data = t.m_data;
+		}
+	}
+	//析构函数 
+	//~t
+	~Test()
+	{
+		cout<<"Free Test Object:"<<this<<endl;
+	}
+public:
+	int GetData()const  //常方法
+	{
+		return m_data;
+	}
+	/*
+	int GetData()
+	{
+		return m_data
+	}
+	*/
+private:
+	int m_data;
+};
+
+void main()
+{
+	Test t(10);   //  t
+	Test t1 = t;  //  t1
+	Test t2;
+	t2 = t1;
+}
+
+/*
+class Test
+{
+public:
+	//构造函数  自动  
+	Test() //默认构造函数
+	{
+		cout<<"Create Test() Object."<<endl;
+	}
+	Test(int data)
+	{
+		cout<<"Create Test(int) Object."<<endl;
+		m_data = data;
+	}
+	Test(int data, int value)
+	{
+		cout<<"Create Test(int,int) Object."<<endl;
+		m_data = data;
+		m_value = value;
+	}
+public:
+	void fun()
+	{}
+private:
+	int m_data; //
+	int m_value;
+};
+
+void main()
+{
+	Test t; //
+	Test t1(10);
+	Test t2(10,20);
+}
+
+/*
+class A
+{
+public:
+	void PrintA()
+	{
+		cout << _a << endl;
+	}
+	void Show()
+	{
+		cout << "Show()" << endl;
+	}
+private:
+	int _a;
+};
+
+
+int main()
+{
+	A a;
+	//A* p = NULL;
+	A *p = &a;
+	p->PrintA();
+	p->Show();
+}
+
+/*
+class CGoods
+{
+public:
+	void RegisterGoods(char *name, int count, float price)
+	{
+		strcpy(this->name, name);
+		this->count = count;
+		this->price = price;
+	}
+	void SetName(char *name)
+	{
+		strcpy(this->name, name);
+	}
+	void SetCount(int count)
+	{
+		this->count = count;
+	}
+	void SetPrice(float price)
+	{
+		this->price = price;
+	}
+public:
+	char* GetName()
+	{
+		return this->name;
+	}
+	int GetCount()
+	{
+		return count;
+	}
+	float GetPrice()
+	{
+		return price;
+	}
+	float GetTotalPrice()
+	{
+		return price*count;
+	}
+private:
+	char name[10];
+	int  count;
+	float price;
+	float total_price;
+};
+//测试
+void main()
+{
+	CGoods c1, c2;
+	//RegisterGoods(&c1, "C++", 10, 35);
+	c1.RegisterGoods("C++", 10, 35);
+	c2.RegisterGoods("C++", 10, 35);
+	//GetName(&c1);
+	cout<<c1.GetName()<<endl;
+	c1.SetName("Java");
+	cout<<c1.GetName()<<endl;
+}
+
+
+/*
+class Test  //1
+{
+public:
+	//void SetData(Test *const this, int data = 0)
+	void SetData(int data = 0)  //2
+	{
+		this->m_data = data;
+	}
+	//int GetData(Test *const this)
+	int GetData()
+	{
+		return this->m_data;   //this?
+	}
+	//void SetValue(Test *const this, int value = 0)
+	void SetValue(int value = 0)  //2
+	{
+		this->m_value = value;
+	}
+	//int GetValue(Test * const this);
+	int GetValue()
+	{
+		return this->m_value;
+	}
+
+public:
+	void ShowData()
+	{
+		//cout<<"data = "<<m_data<<endl;
+		printf("data = %d, value = %d\n",m_data, m_value);
+	}
+	void ListData()
+	{
+		printf("data = %d, value = %d\n",m_data, m_value);
+	}
+public:
+	int m_data;   //2
+	int m_value;
+};
+
+void main()
+{
+	Test t1, t2;
+	t1.SetData(1); //SetData(&t1, 1);
+	t1.SetValue(2);
+	t2.SetData(10);//SetData(&t2, 10);
+	t2.SetValue(20);
+
+	cout<<"t1 data = "<<t1.GetData()<<endl;   //GetData(&t1);
+	cout<<"t2 data = "<<t2.GetData()<<endl;
+
+}
+
+/*
+struct Student
+{
+	char name[10];
+	char sex[3];
+	int age;
+	int height;
+	int weight;
+};
+//////////////////////////////////////////////
+void InitStu(Student *_this)
+{
+	_this->name[0] = '\0';
+	_this->sex[0] = '\0';
+	_this->age = 0;
+	_this->height = 0;
+	_this->weight = 0;
+}
+char *GetName(Student *_this)
+{
+	return _this->name;
+}
+
+void main()
+{
+	Student s1,s2;
+	InitStu(&s1);
+	InitStu(&s2);
+	char *n1 = GetName(&s1);
+	char *n2 = GetName(&s2);
+}
+
+
+/*
+//先定义后使用
+
+
+/*
+void main()
+{
+	cout<<sizeof(Test)<<endl;
+}
+
+/*
+void main()
+{
+	Test t1, t2;
+	t1.SetData(1);
+	t1.SetValue(2);
+	t2.SetData(10);
+	t2.SetValue(20);
+
+	cout<<"t1 data = "<<t1.GetData()<<endl;
+	cout<<"t2 data = "<<t2.GetData()<<endl;
+}
+
+
+/*
+void main()
+{
+	Test t;  //开空间
+	t.m_data = 10;
+
+	Test.m_data = 10;
+	Test::m_data = 100;
+}
+
+
+/*
+void main()
+{
+	Test t;
+	t.SetData();
+	//int value = t.GetData();
+	//cout<<"t = "<<t<<endl;
+	t.ShowData();
+}
+
+
+/*
 void main()
 {
 	CGoods c1, c2;
